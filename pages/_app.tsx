e67f9ac85d13ce0c2e2ba32ffd1dea173e39419d
Josh <josh@joshtaylor.io>
2023-07-17 10:03:44 -0400

    test case

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google';
+import { ReactElement } from 'react';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const DefaultLayout = (children: ReactElement) => children;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{}}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
